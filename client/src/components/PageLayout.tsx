01ef90f21435a4497aaa96acc67ceb5bf1610756
Oluwamayowa Esan <25509915+Battlesquid@users.noreply.github.com>
2023-12-03 16:25:27 -0700

    fix eslint issues

--- client/src/components/PageLayout.tsx
+++ client/src/components/PageLayout.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import {makeStyles} from 'tss-react/mui';
 import {Outlet} from "react-router-dom";
 import {Header} from "./Header";
-import {Link} from "@mui/material";
 import {Footer} from "./Footer";
 
 const useStyles = makeStyles()({
